3caa37a34464393f1bd0372f0adc4350251a21e0
Miles Morris <milesmorris123@gmail.com>
2023-10-19 13:36:18 +0100

    built form and method for capturing text from the form in a state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [details, setDetails] = useState({
+    name: '',
+    email: '',
+    phone:''
+  })
+
+  function handleInputText(e){
+    setDetails({ ...details, [e.target.name]: e.target.value });
+    console.log(details)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='form-div'>
+      <h1>Please Enter Your Details</h1>
+      <div>
+        <h4>Name</h4>
+        <input 
+          type="text"
+          value={details.name}
+          onChange={handleInputText}
+        ></input>
+      </div>
+      <div>
+        <h4>Email Address</h4>
+        <input 
+          type="text"
+          value={details.email}
+          onChange={handleInputText}
+          ></input>
+      </div>
+      <div>
+        <h4>Phone Number</h4>
+        <input 
+        type="text"
+        value={details.phone}
+        onChange={handleInputText}
+        ></input>
+      </div>
+      <div>
+        <form>
+          <button>Submit</button>
+        </form>
+      </div>
+      </div>
     </div>
   );
 }
